3ae9c6ccc6f72bcee3075139d7363d1634fa2a54
Guilherme Francisco <guisanfrancisco@gmail.com>
2021-04-14 10:43:29 -0300

    Removing order data insertion

--- src/main/java/br/com/guisfco/onlineshopping/job/CleanDatabaseJob.java
+++ src/main/java/br/com/guisfco/onlineshopping/job/CleanDatabaseJob.java
@@ -54,18 +54,18 @@ public class CleanDatabaseJob {
 
         Customer customer = new Customer();
 
-        for (int i = 0; i < nextInt(1, 6); i++) {
+        for (int i = 0; i < nextInt(2, 6); i++) {
             customer = saveCustomerService.save(CustomerRequestFixture.get().random().build());
         }
 
         Product product = new Product();
 
-        for (int i = 0; i < nextInt(1, 6); i++) {
+        for (int i = 0; i < nextInt(2, 6); i++) {
             product = saveProductService.save(ProductRequestFixture.get().random().build());
         }
-
-        final OrderRequest orderRequest = new OrderRequest(customer.getId(), Collections.singletonList(new OrderProduct(product.getId(), nextInt(1, 11))));
-
-        saveOrderService.save(orderRequest);
+//
+//        final OrderRequest orderRequest = new OrderRequest(customer.getId(), Collections.singletonList(new OrderProduct(product.getId(), nextInt(1, 11))));
+//
+//        saveOrderService.save(orderRequest);
     }
 }
