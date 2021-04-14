44598eb772f129a3282ff7621abfeed6a72dba40
Guilherme Francisco <guisanfrancisco@gmail.com>
2021-04-14 12:28:50 -0300

    Removing order mock data

--- src/main/java/br/com/guisfco/onlineshopping/job/CleanDatabaseJob.java
+++ src/main/java/br/com/guisfco/onlineshopping/job/CleanDatabaseJob.java
@@ -1,25 +1,17 @@
 package br.com.guisfco.onlineshopping.job;
 
-import br.com.guisfco.onlineshopping.domain.OrderProduct;
-import br.com.guisfco.onlineshopping.domain.OrderRequest;
-import br.com.guisfco.onlineshopping.entity.Customer;
-import br.com.guisfco.onlineshopping.entity.Product;
 import br.com.guisfco.onlineshopping.fixture.CustomerRequestFixture;
 import br.com.guisfco.onlineshopping.fixture.ProductRequestFixture;
 import br.com.guisfco.onlineshopping.repository.CustomerRepository;
 import br.com.guisfco.onlineshopping.repository.OrderRepository;
 import br.com.guisfco.onlineshopping.repository.ProductRepository;
 import br.com.guisfco.onlineshopping.service.customer.SaveCustomerService;
-import br.com.guisfco.onlineshopping.service.order.SaveOrderService;
 import br.com.guisfco.onlineshopping.service.product.SaveProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.Collections;
-import java.util.List;
 
 import static br.com.guisfco.onlineshopping.util.RandomUtils.nextInt;
 
@@ -39,8 +31,6 @@ public class CleanDatabaseJob {
 
     private final SaveProductService saveProductService;
 
-    private final SaveOrderService saveOrderService;
-
     /**
      * Executa a limpeza dos registros da base de dados a cada 3 horas
      */
@@ -57,11 +47,6 @@ public class CleanDatabaseJob {
         insertProductMockData();
     }
 
-    @Scheduled(fixedDelay = 10805000)
-    public void executeOrderMockData() {
-        insertOrderMockData();
-    }
-
     private void insertCustomerMockData() {
 
         for (int i = 0; i < nextInt(1, 6); i++) {
@@ -73,22 +58,6 @@ public class CleanDatabaseJob {
 
         for (int i = 0; i < nextInt(1, 6); i++) {
             saveProductService.save(ProductRequestFixture.get().random().build());
-        }
-    }
-
-    private void insertOrderMockData() {
-
-        final List<Customer> customers = customerRepository.findAll();
-        final List<Product> products = productRepository.findAll();
-
-        for (int i = 0; i < nextInt(1, 6); i++) {
-
-            final Long customerId = customers.get(nextInt(0, customers.size())).getId();
-            final Long productId = products.get(nextInt(0, products.size())).getId();
-
-            final OrderRequest orderRequest = new OrderRequest(customerId, Collections.singletonList(new OrderProduct(productId, nextInt(1, 6))));
-
-            saveOrderService.save(orderRequest);
         }
     }
 }
